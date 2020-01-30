4fdeed69f7f93a8eb6580dd3384635550f330b30
CatalinaSorina <gherzan.catalina@yahoo.com>
2020-01-30 17:58:38 +0200

    adding bar charts

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # Charts and map
+
 Creating React app with nivo.rocks charts and map styles.
+
+Install:  
+npm i nivo
--- src/App.js
+++ src/App.js
@@ -1,24 +1,18 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import Bar from "./components/Bar";
+import { bardata } from "./components/data";
+
+// console.log(bardata)
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <header className="App-header">Charts and map</header>
+      <div className="charts">
+        <Bar data={bardata} horizontal={"horizontal"} />
+        <Bar data={bardata} />
+      </div>
     </div>
   );
 }
