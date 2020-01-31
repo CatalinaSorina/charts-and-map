2d47b60ecec4cfcf86a3804e8918e91b6b50b4bd
CatalinaSorina <gherzan.catalina@yahoo.com>
2020-01-31 13:59:04 +0200

    adding Bar charts

--- src/App.js
+++ src/App.js
@@ -1,18 +1,12 @@
 import React from 'react';
 import './App.css';
-import Bar from "./components/Bar";
-import { bardata } from "./components/data";
-
-// console.log(bardata)
+import Charts from "./components/Charts";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">Charts and map</header>
-      <div className="charts">
-        <Bar data={bardata} horizontal={"horizontal"} />
-        <Bar data={bardata} />
-      </div>
+      <Charts />
     </div>
   );
 }
